be027e5c5aa0b4b0fa9965c51098bb547777b6f3
marie flores <marie.flores@filigran.io>
2023-09-12 16:27:56 +0200

    [backend] add test on searching

--- opencti-platform/opencti-graphql/tests/02-integration/01-database/index-file-test.js
+++ opencti-platform/opencti-graphql/tests/02-integration/01-database/index-file-test.js
@@ -1,6 +1,6 @@
-import { describe, expect, it } from 'vitest';
+import { afterAll, describe, expect, it } from 'vitest';
 import { createReadStream } from 'node:fs';
-import { elDelete, elIndexFile, elLoadById } from '../../../src/database/engine';
+import { elDelete, elIndexFile, elLoadById, elSearchFiles } from '../../../src/database/engine';
 import { ADMIN_USER, testContext } from '../../utils/testQuery';
 import { deleteFile, getFileContent, upload } from '../../../src/database/file-storage';
 import { INDEX_FILES } from '../../../src/database/utils';
@@ -25,30 +25,53 @@ const testFileIndexing = async (fileName, mimetype, documentId) => {
 
   // Assertions
   expect(document.attachment).not.toBeNull();
+  expect(document.attachment.content).not.toBeNull();
   expect(document.attachment.content_type.includes(file.mimetype)).toBeTruthy();
 
-  // cleanup : delete file in minio and elastic
+  // cleanup : delete file in minio
   await deleteFile(testContext, ADMIN_USER, uploadedFile.id);
-  await elDelete(INDEX_FILES, documentId);
+
+  // TODO method should return document each test should assert specificly on it
 };
 
+const testFilesSearching = async (search, expectedFilesIds) => {
+  const data = await elSearchFiles(testContext, ADMIN_USER, { search });
+  expect(data).not.toBeNull();
+  expect(data.edges.length).toEqual(expectedFilesIds.length);
+  const resultIds = data.edges.map((edge) => edge.node.id);
+  expect(resultIds).toEqual(expectedFilesIds);
+};
+
+const filesIds = [];
 describe('Indexing file test', () => {
+  afterAll(async () => {
+    // cleanup : delete file in elastic
+    await Promise.all(filesIds.map(async ({ fileId }) => {
+      await elDelete(INDEX_FILES, fileId);
+    }));
+  });
   it('Should index small pdf file', async () => {
     await testFileIndexing('test-report-to-index.pdf', 'application/pdf', 'TEST_FILE_1');
+    filesIds.push('TEST_FILE_1');
   });
   it('Should index large pdf file', async () => {
     await testFileIndexing('test-large-report-to-index.pdf', 'application/pdf', 'TEST_FILE_2');
+    filesIds.push('TEST_FILE_2');
   });
   it('Should index txt file', async () => {
     await testFileIndexing('test-file-to-index.txt', 'text/plain', 'TEST_FILE_3');
+    filesIds.push('TEST_FILE_3');
   });
   it('Should index csv file', async () => {
     await testFileIndexing('test-file-to-index.csv', 'text/plain', 'TEST_FILE_4');
+    filesIds.push('TEST_FILE_4');
   });
   it('Should index xls file', async () => {
     await testFileIndexing('test-file-to-index.xls', 'application/vnd.ms-excel', 'TEST_FILE_5');
+    filesIds.push('TEST_FILE_5');
   });
-  it('Should find document by search query', () => {
-    // imlement test
+  it('Should find document by search query', async () => {
+    await testFilesSearching('elastic', ['TEST_FILE_1']);
+    await testFilesSearching('control', ['TEST_FILE_4', 'TEST_FILE_2']);
   });
 });
